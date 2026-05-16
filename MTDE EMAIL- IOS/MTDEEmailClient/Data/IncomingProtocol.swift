ff752686c998395daa826fa0c0248f447ea46df8
hamdan-cts <muhamad.hamdan@cts.ps>
2026-05-16 13:00:29 +0300

    Delete MTDE EMAIL- IOS/MTDEEmailClient directory

--- MTDE EMAIL- IOS/MTDEEmailClient/Data/IncomingProtocol.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-/// Matches Android `IncomingProtocol.name` (IMAP / POP3).
-enum IncomingProtocol: String, Codable, CaseIterable, Identifiable {
-    case IMAP
-    case POP3
-
-    var id: String { rawValue }
-}
